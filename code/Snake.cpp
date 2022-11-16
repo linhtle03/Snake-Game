8d132cbd31c61554cad8eba6130d1f507f89dbbb
admin <admin@BCS-Ubuntu-08.lrccd.ad.losrios.edu>
2022-11-16 10:48:34 -0800

    update function

--- code/Snake.cpp
+++ code/Snake.cpp
@@ -1,7 +1,11 @@
 #include "Snake.h"
 Snake::Snake(float startX, float startY)
 {
+    m_Position.x = startX;
+	m_Position.y = startY;
 
+	m_Shape.setSize(sf::Vector2f(50, 5));
+	m_Shape.setPosition(m_Position);
 }
 
 FloatRect Snake::getPosition()
@@ -35,5 +39,17 @@ void Snake::moveDown()
 
 void Snake::update(Time dt)
 {
-    
+    if (m_MovingLeft) {
+		m_Position.x -= m_Speed * dt.asSeconds();
+	}
+	if (m_MovingRight) {
+		m_Position.x += m_Speed * dt.asSeconds();
+	}
+    if (m_MovingDown) {
+		m_Position.y -= m_Speed * dt.asSeconds();
+	}
+	if (m_MovingUp) {
+		m_Position.y += m_Speed * dt.asSeconds();
+	}
+	m_Shape.setPosition(m_Position);
 }
