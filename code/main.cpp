a9ed28bd7b44c102e4165233cf69dcdbcd3c7daa
admin <admin@BCS-Ubuntu-31.lrccd.ad.losrios.edu>
2022-12-04 12:52:20 -0800

    more png

--- code/main.cpp
+++ code/main.cpp
@@ -81,7 +81,7 @@ int main()
 	Texture textureBackground;
 
 	// Load a graphic into the texture
-	textureBackground.loadFromFile("graphics/grass1.png");
+	textureBackground.loadFromFile("graphics/grass.png");
 
 	// Create a sprite
 	Sprite spriteBackground;
